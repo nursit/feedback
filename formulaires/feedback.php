4408e5a3027cf154b7f7c044181ef5827b70f304
Cerdic <cedric@yterium.com>
2020-02-17 12:24:30 +0100

    mise a jour de la lib WhichBrowser

--- formulaires/feedback.php
+++ formulaires/feedback.php
@@ -36,21 +36,25 @@ function formulaires_feedback_charger_dist($destinataires = null){
 	}
 
 	// ajouter l'appel a WichBrowser
-	$dir = protocole_implicite(url_absolue(find_in_path("lib/WhichBrowser/")));
+	$dir = protocole_implicite(url_absolue(find_in_path("lib/WhichBrowser/Server/")));
 	$js = <<<snipet
 <script>
-  (function(){var p=[],w=window,d=document,e=f=0;p.push('ua='+encodeURIComponent(navigator.userAgent));e|=w.ActiveXObject?1:0;e|=w.opera?2:0;e|=w.chrome?4:0;
-  e|='getBoxObjectFor' in d || 'mozInnerScreenX' in w?8:0;e|=('WebKitCSSMatrix' in w||'WebKitPoint' in w||'webkitStorageInfo' in w||'webkitURL' in w)?16:0;
-  e|=(e&16&&({}.toString).toString().indexOf("\\n")===-1)?32:0;p.push('e='+e);f|='sandbox' in d.createElement('iframe')?1:0;f|='WebSocket' in w?2:0;
-  f|=w.Worker?4:0;f|=w.applicationCache?8:0;f|=w.history && history.pushState?16:0;f|=d.documentElement.webkitRequestFullScreen?32:0;f|='FileReader' in w?64:0;
-  p.push('f='+f);p.push('r='+Math.random().toString(36).substring(7));p.push('w='+screen.width);p.push('h='+screen.height);var s=d.createElement('script');
-  s.src='{$dir}detect.js?' + p.join('&');d.getElementsByTagName('head')[0].appendChild(s);})();
-  function detectBrowserInit(){
-    if (typeof WhichBrowser=="undefined") {setTimeout(detectBrowserInit,250);return}
-	  Browsers = new WhichBrowser();
-	  jQuery('#browserInfos').attr('value',JSON.stringify(Browsers));
-  }
-  jQuery(detectBrowserInit);
+	(function(){var p=[],w=window,d=document,e=f=0;p.push('ua='+encodeURIComponent(navigator.userAgent));e|=w.ActiveXObject?1:0;e|=w.opera?2:0;e|=w.chrome?4:0;
+	e|='getBoxObjectFor' in d || 'mozInnerScreenX' in w?8:0;e|=('WebKitCSSMatrix' in w||'WebKitPoint' in w||'webkitStorageInfo' in w||'webkitURL' in w)?16:0;
+	e|=(e&16&&({}.toString).toString().indexOf("\\n")===-1)?32:0;p.push('e='+e);f|='sandbox' in d.createElement('iframe')?1:0;f|='WebSocket' in w?2:0;
+	f|=w.Worker?4:0;f|=w.applicationCache?8:0;f|=w.history && history.pushState?16:0;f|=d.documentElement.webkitRequestFullScreen?32:0;f|='FileReader' in w?64:0;
+	p.push('f='+f);p.push('r='+Math.random().toString(36).substring(7));p.push('w='+screen.width);p.push('h='+screen.height);var s=d.createElement('script');
+	s.src='{$dir}detect.js?' + p.join('&');d.getElementsByTagName('head')[0].appendChild(s);})();
+	function detectBrowserInit(){
+		if (typeof WhichBrowser=="undefined") {setTimeout(detectBrowserInit,250);return}
+		var result = new WhichBrowser();
+		result = JSON.stringify(result);
+		result = JSON.parse(result);
+		result.device.screenWidth = screen.width;
+		result.device.screenHeight = screen.height;
+		jQuery('#browserInfos').attr('value',JSON.stringify(result));
+	}
+	jQuery(detectBrowserInit);
 </script>
 <input type='hidden' name='browserInfos' value='' id='browserInfos' />
 snipet;
